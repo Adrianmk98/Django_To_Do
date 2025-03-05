321b399cae56717b3173ce9c959e7a7d7820e4ab
Adrian <adrianmkrzyzanowski@gmail.com>
2025-03-05 15:33:04 -0500

    Added delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('toggle/<int:task_id>/', views.toggle_task, name='toggle_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
     path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
+    path('delete_all/', views.empty_tasks, name='delete_all_tasks'),
 ]
